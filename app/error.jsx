bf47503c57a8b1946de96e02369c3a60c88999ec
rianwp <rianwp2015@gmail.com>
2023-04-09 17:12:14 +0700

    fix error page 4

--- app/error.jsx
+++ app/error.jsx
@@ -1,9 +1,11 @@
 "use client"
+
+
 const Error = ({error, reset}) => {
   return (
     <div className="w-full -mt-14 landscape:h-screen hero-responsive flex items-center justify-center bg-gray-100">
       <div className="flex-col flex">
-        <div className="text-5xl font-extrabold">Terjadi kesalahan</div>
+        <div className="text-5xl font-extrabold mb-5">Terjadi kesalahan</div>
         <div className="self-end">
           <button className="bg-red-500 text-white hover:bg-red-600 transition duration-300 block py-2 px-3 rounded-lg" onClick={() => reset()} >
             Try again
