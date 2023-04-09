2956e2c0bf5ab7be141cb41da02a9c875bdf9658
rianwp <rianwp2015@gmail.com>
2023-04-09 20:21:47 +0700

    team photo

--- components/Landing/Team/index.jsx
+++ components/Landing/Team/index.jsx
@@ -9,7 +9,7 @@ const Team = () => {
         <div className="flex flex-row mt-5 w-full justify-center flex-wrap items-start">
           <Member name="Hanief Fadilul Haq" deskripsi="Social Media Manager" img=""/>
           <Member name="Adrian Wahyu Prasetya" deskripsi="Web Developer" img=""/>
-          <Member name="Adham Faraitodi" deskripsi="Webmaster" img=""/>
+          <Member name="Adham Faraitodi" deskripsi="Webmaster" img="img/adham.jpg"/>
           <Member name="Bagas Ardianto" deskripsi="Content Writer" img=""/>
           <Member name="Muhammad Jaelani AS" deskripsi="Content Creator" img=""/>
           <Member name="Muhammad Alfian SA" deskripsi="Content Creator" img=""/>
