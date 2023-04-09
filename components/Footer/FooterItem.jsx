2dc02fd83d6f1178c394297344ab4c719ad31e17
rianwp <rianwp2015@gmail.com>
2023-04-09 20:14:39 +0700

    footer link new tab

--- components/Footer/FooterItem.jsx
+++ components/Footer/FooterItem.jsx
@@ -1,7 +1,7 @@
 const FooterItem = ({children, href}) => {
   return (
       href ? 
-      <a href={href} className="text-white opacity-80 hover:opacity-100 transition duration-300 inline-flex flex-row items-center space-x-2 break-all max-w-fit">
+      <a href={href} target="_blank" className="text-white opacity-80 hover:opacity-100 transition duration-300 inline-flex flex-row items-center space-x-2 break-all max-w-fit">
         {children}
       </a>
       :
