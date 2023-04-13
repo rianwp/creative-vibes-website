e60663cfd55ee2034e3f6ba926546542abe22813
rianwp <rianwp2015@gmail.com>
2023-04-13 21:18:46 +0700

    fix hanief

--- components/Landing/Team/index.jsx
+++ components/Landing/Team/index.jsx
@@ -7,7 +7,7 @@ const Team = () => {
       <div className="py-32 lg:w-3/4 md:w-full sm:w-full w-full mx-auto sm:px-8 px-4">
         <SectionTitle>Members of The Team</SectionTitle>
         <div className="flex flex-row mt-5 w-full justify-center flex-wrap items-start">
-          <Member name="Hanief Fadilul Haq" deskripsi="Social Media Manager" img="/img/hanief.jpg"/>
+          <Member name="Hanief Fadilul Haq" deskripsi="Social Media Manager" img="/img/hanief.JPG"/>
           <Member name="Adrian Wahyu Prasetya" deskripsi="Web Developer" img="/img/adrian.jpg"/>
           <Member name="Adham Faraitodi" deskripsi="Webmaster" img="/img/adham.jpeg"/>
           <Member name="Bagas Ardianto" deskripsi="Content Writer" img="/img/bagas.png"/>
