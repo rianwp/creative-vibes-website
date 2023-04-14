d5be2f009e2ad86276e71f1911555463a58f3992
rianwp <rianwp2015@gmail.com>
2023-04-14 08:00:26 +0700

    vercel analytics

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
+import { Analytics } from "@vercel/analytics/react"
 
 const Layout = ({ children }) => {
   return (
@@ -8,6 +9,7 @@ const Layout = ({ children }) => {
         <Navbar/>
         <main>
           {children}
+          <Analytics/>
         </main>
         <Footer/>
       </body>
