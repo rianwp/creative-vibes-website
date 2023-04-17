369fe7d22110baba3e9ab1b9bd20461d79f1207b
rianwp <rianwp2015@gmail.com>
2023-04-17 21:53:44 +0700

    test

--- components/Contents/Content/MainContent.jsx
+++ components/Contents/Content/MainContent.jsx
@@ -14,7 +14,7 @@ const MainContent = ({tanggal, img, alt, text, tags}) => {
         )}
       </div>
       <div className="w-full aspect-video rounded-lg overflow-hidden mb-5">
-        <img src={img} alt={alt} className="w-full object-cover object-center" referrerPolicy="no-referrer"/>
+        <img alt={alt} src={img} className="w-full object-cover object-center" referrerPolicy="no-referrer"/>
       </div>
       <div className="text-justify wordpress leading-loose" dangerouslySetInnerHTML={{__html: text}}/>
     </div>
