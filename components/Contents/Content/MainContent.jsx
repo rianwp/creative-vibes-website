400bfd3b16071bc5eb68b4c85709a0a95d2e311d
rianwp <90314741+rianwp@users.noreply.github.com>
2023-04-22 17:43:14 +0700

    Update MainContent.jsx

--- components/Contents/Content/MainContent.jsx
+++ components/Contents/Content/MainContent.jsx
@@ -4,13 +4,15 @@ import TagBox from "../TagBox"
 const MainContent = ({tanggal, img, alt, text, tags}) => {
   return (
     <div className="mt-5 w-full">
-      <div className="flex flex-row items-center space-x-2 mb-2 flex-wrap">
-        <div className="flex flex-row items-center space-x-2 text-gray-400">
+      <div className="flex flex-row items-center mb-2 flex-wrap">
+        <div className="flex flex-row items-center space-x-2 text-gray-400 mr-2 mb-2">
           <AiFillCalendar/>
           <div>{tanggal}</div>
         </div>
         {tags.map((tag)=>
-          <TagBox key={tag.id}>{tag.name}</TagBox> 
+          <div key={tag.id} className="mr-2 mb-2">
+            <TagBox>{tag.name}</TagBox>
+          </div> 
         )}
       </div>
       <div className="w-full aspect-video rounded-lg overflow-hidden mb-5">
