82e84fab7aea2211a8eee6b4f4dfb25b9d7ade22
rianwp <rianwp2015@gmail.com>
2023-04-26 16:58:35 +0700

    center notfound

--- components/Contents/Content/NotFound.jsx
+++ components/Contents/Content/NotFound.jsx
@@ -5,7 +5,7 @@ const NotFound = () => {
     <div className="-mt-14 w-full h-screen flex items-center justify-center">
       <div className="flex flex-col justify-center items-center">
         <RiArticleLine className="w-32 h-32 text-gray-400"/>
-        <div className="text-xl font-semibold text-gray-400">
+        <div className="text-xl font-semibold text-gray-400 text-center">
           Artikel Yang Anda Cari Tidak Ditemukan
         </div>
       </div>
