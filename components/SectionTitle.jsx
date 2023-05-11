442558b9e03ee603ae80527fac661a8ba0f1121a
Adrian Wahyu Prasetya <90314741+rianwp@users.noreply.github.com>
2023-05-11 14:13:58 +0700

    Update SectionTitle.jsx

--- components/SectionTitle.jsx
+++ components/SectionTitle.jsx
@@ -1,6 +1,6 @@
 const SectionTitle = ({children}) => {
   return (
-    <div className="text-black text-2xl font-bold tracking-widest text-center">{children}</div>
+    <h1 className="text-black text-2xl font-bold tracking-widest text-center">{children}</h1>
   )
 }
 
